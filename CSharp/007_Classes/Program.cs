a1ee6226e4cff76b55ac84180c95106cd08d2ee6
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-06 13:10:49 +0300

    Classes completed!

---
+++ CSharp/007_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Ramazan";
+            customer.LastName = "ALTINTOP";
+            customer.City = "Ankara";
+
+            Customer customer2 = new Customer()
+            {
+                Id = 2, FirstName = "Cem", LastName = "Doe", City = "Rize"
+            };
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+}
