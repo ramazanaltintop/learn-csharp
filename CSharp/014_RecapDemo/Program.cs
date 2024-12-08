ca91554f30fff3f7305a6d4bc0e3805913dabdfd
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-08 13:20:08 +0300

    Recap Demo completed!

---
+++ CSharp/014_RecapDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_RecapDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.Logger = new DatabaseLogger();
+            //customerManager.Logger = new FileLogger();
+            customerManager.Logger = new SmsLogger();
+            customerManager.Add();
+        }
+    }
+    class CustomerManager
+    {
+        //Property Injection
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added!...");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database!...");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file!...");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to sms!...");
+        }
+    }
+}
