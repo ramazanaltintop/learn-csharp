a02ffdc97e9069dceda4070c82618e89346a6d60
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-08 12:59:01 +0300

    Abstract classes completed!

---
+++ CSharp/013_AbstractClasses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new SqlServer();
+            database.Add();
+            database.Delete();
+
+            Database database2 = new Oracle();
+            database2.Add();
+            database2.Delete();
+        }
+    }
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added by default!...");
+        }
+        public abstract void Delete();
+    }
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by SqlServer!...");
+        }
+    }
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle!...");
+        }
+    }
+}
