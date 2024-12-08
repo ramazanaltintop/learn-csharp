4d76f15ac6430b5f86a44f049ac3e083c639ec40
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-08 14:46:04 +0300

    Access Modifiers completed!

---
+++ CSharp/015_AccessModifiers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Customer
+    {
+        private int Id { get; set; }
+        protected int CustomerId { get; set; }
+        public void Save()
+        {
+            Id += 1;
+        }
+        public void Delete()
+        {
+            Id -= -1;
+        }
+    }
+    class Student : Customer
+    {
+        public void Save2()
+        {
+            Customer customer = new Customer();
+            CustomerId = 1;
+        }
+    }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        private class NestedClass
+        {
+
+        }
+    }
+}
