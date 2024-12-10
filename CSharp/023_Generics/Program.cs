9f8744b5959f1cac72f2731c10abec57f8c3f83f
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-10 11:38:42 +0300

    Generics completed!

---
+++ CSharp/023_Generics/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _023_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Ankara", "Kırıkkale", "Kırşehir", "Afyon");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Customer> customers = utilities.BuildList<Customer>(
+                new Customer { FirstName = "Ramazan" },
+                new Customer { FirstName = "Cemre" }
+            );
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+        }
+    }
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+    class Product : IEntity
+    {
+
+    }
+    interface IProductDal : IRepository<Product>
+    {
+
+    }
+    class Customer : IEntity
+    {
+        public string FirstName { get; set; }
+    }
+    interface ICustomerDal : IRepository<Customer>
+    {
+
+    }
+    class Student : IEntity
+    {
+
+    }
+    interface IEntity
+    {
+
+    }
+    interface IStudentDal : IRepository<Student>
+    {
+
+    }
+    interface IRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T update);
+    }
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
