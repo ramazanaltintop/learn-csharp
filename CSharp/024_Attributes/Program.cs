e26262016968e81c13dac93b6ca8c0f68286edae
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-10 12:52:46 +0300

    Attributes completed!

---
+++ CSharp/024_Attributes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _024_Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, LastName = "Demir", Age = 33 };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+        }
+    }
+    [ToTable("Customers")]
+    [ToTable("TblCustomers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0} {1} {2} {3} added!...", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0} {1} {2} {3} added!...", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+}
