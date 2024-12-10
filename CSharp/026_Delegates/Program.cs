b2470418380c358a1787faf124c0f66f28abadb4
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-10 15:15:31 +0300

    Delegates completed!

---
+++ CSharp/026_Delegates/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _026_Delegates
+{
+    internal class Program
+    {
+        public delegate void MyDelegate();
+        public delegate void MyDelegate2(string text);
+        public delegate int MyDelegate3(int number1, int number2);
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+            myDelegate -= customerManager.SendMessage;
+            myDelegate();
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage2;
+            myDelegate2 += customerManager.ShowAlert2;
+            myDelegate2("Hello!...");
+
+            Operations operations = new Operations();
+            MyDelegate3 myDelegate3 = operations.Add;
+            myDelegate3 += operations.Multiply;
+            var result = myDelegate3(15, 35);
+            Console.WriteLine(result);
+        }
+    }
+    public class CustomerManager
+    {
+        public void SendMessage()
+        {
+            Console.WriteLine("Hello!...");
+        }
+        public void ShowAlert()
+        {
+            Console.WriteLine("Be careful!...");
+        }
+        public void SendMessage2(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public void ShowAlert2(string alert)
+        {
+            Console.WriteLine(alert);
+        }
+    }
+    class Operations
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
