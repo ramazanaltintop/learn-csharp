414e8b3bf96f77b6ba06dddaeb87b8d45e573293
Ramazan Altintop <ramazanaltintop@tutanota.com>
2024-12-10 15:41:35 +0300

    Func completed!

---
+++ CSharp/027_Func/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _027_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example1
+            Func<int, int, int> add = Add;
+            Console.WriteLine(add(25, 25));
+
+            //Example2
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+            Console.WriteLine(getRandomNumber());
+
+            //Example3
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+            Console.WriteLine(getRandomNumber2());
+        }
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
