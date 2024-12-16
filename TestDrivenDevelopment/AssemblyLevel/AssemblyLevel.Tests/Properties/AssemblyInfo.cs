6ce668f0db9595831244c0429916b18fd0cf2f92
Ramazan Altintop <ramazan.altintop@yahoo.com>
2024-12-16 14:01:09 +0300

    Assembly Level completed!

---
+++ TestDrivenDevelopment/AssemblyLevel/AssemblyLevel.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssemblyLevel.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssemblyLevel.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26e9bb73-d0ef-41f0-ac1a-c235dbc09636")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDrivenDevelopment/AssemblyLevel/AssemblyLevel.Tests/UnitTest2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLevel.Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            Debug.WriteLine("Running UnitTest2 ClassInitialize");
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            Debug.WriteLine("Running UnitTest2 ClassCleanup");
+        }
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("Running UnitTest2 TestInitialize");
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("Running UnitTest2 TestCleanup");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Debug.WriteLine("Running TestMethod3");
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Debug.WriteLine("Running TestMethod4");
+        }
+    }
+}
