443af97f0548a68a4ff91b0628acb5cc119efff8
Ramazan Altintop <ramazan.altintop@yahoo.com>
2024-12-16 15:23:19 +0300

    ExpectedException completed!

---
+++ TestDrivenDevelopment/ExpectedException/ShoppingCart.Tests/CartTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private static CartManager _cartManager;
+        private static CartItem _cartItem;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 25500
+                },
+                Quantity = 1
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod]
+        public void Sepete_farkli_urunden_1_adet_eklendiginde_sepetteki_toplam_urun_adedi_ve_eleman_sayisi_birer_artmalidır()
+        {
+            //Arrange
+            int totalQuantity = _cartManager.TotalQuantity;
+            int totalItems = _cartManager.TotalItems;
+
+            //Act
+            _cartManager.Add(new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 2,
+                    ProductName = "Mouse",
+                    UnitPrice = 500
+                },
+                Quantity = 1
+            });
+
+            int expectedTotalQuantity = totalQuantity + 1;
+            int expectedTotalItems = totalItems + 1;
+
+            //Assert
+            Assert.AreEqual(expectedTotalItems, _cartManager.TotalItems);
+            Assert.AreEqual(expectedTotalQuantity, _cartManager.TotalQuantity);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), AllowDerivedTypes = true)]
+        public void Sepette_ayni_urunden_ikinci_kez_eklendiginde_hata_vermelidir()
+        {
+            _cartManager.Add(_cartItem);
+        }
+    }
+}
---
+++ TestDrivenDevelopment/ExpectedException/ShoppingCart.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e34b096e-efdf-4943-9021-08a173a7c9b7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
