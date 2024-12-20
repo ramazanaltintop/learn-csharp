49f7b7f0274445c38892b8a34542842df00d2956
Ramazan Altintop <ramazan.altintop@yahoo.com>
2024-12-20 11:43:40 +0300

    TestContextDemo completed!

---
+++ TestDrivenDevelopment/TestContextDemo/TestContextDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestContextDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestContextDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24506233-beb2-44c3-9b70-502f4b580b5e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDrivenDevelopment/TestContextDemo/TestContextDemo.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestContextDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            TestContext.WriteLine("--TestInitialize--\n");
+            TestContext.WriteLine("Test adı : {0}", TestContext.TestName);
+            TestContext.WriteLine("Test durumu : {0}", TestContext.CurrentTestOutcome);
+            TestContext.Properties["info"] = "This is extra information";
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            TestContext.WriteLine("--TestCleanup--\n");
+            TestContext.WriteLine("Test adı : {0}", TestContext.TestName);
+            TestContext.WriteLine("Test durumu : {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test bilgi : {0}", TestContext.Properties["info"]);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("--TestMethod1--\n");
+            TestContext.WriteLine("Test adı : {0}", TestContext.TestName);
+            TestContext.WriteLine("Test durumu : {0}", TestContext.CurrentTestOutcome);
+            TestContext.WriteLine("Test sınıfı : {0}", TestContext.FullyQualifiedTestClassName);
+            TestContext.WriteLine("Test bilgi : {0}", TestContext.Properties["info"]);
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
