2f44c14e416d6e5ef36913e8b9ab6be2c7319aff
Ramazan Altintop <ramazan.altintop@yahoo.com>
2024-12-23 12:27:27 +0300

    Test Attributes 1 completed!

---
+++ TestDrivenDevelopment/TestAttributes1/TestAttributes1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAttributes1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAttributes1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25d4c644-4aac-4a3f-aafa-1cb841a65500")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDrivenDevelopment/TestAttributes1/TestAttributes1.Tests/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestAttributes1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Owner("Ramazan")]
+        [TestCategory("Tester")]
+        [Priority(1)]
+        [TestProperty("Guncelleyen","Cem")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Ramazan")]
+        [TestCategory("Tester")]
+        [Priority(1)]
+        [TestProperty("Guncelleyen","Cem")]
+        [TestProperty("Guncelleyen2","Ali")]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Ramazan")]
+        [TestCategory("Tester")]
+        [TestCategory("Developer")]
+        [TestCategory("Demo")]
+        [Priority(1)]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Ahmet")]
+        [TestCategory("Developer")]
+        [Priority(2)]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        [Owner("Ahmet")]
+        [TestCategory("Developer")]
+        [Priority(2)]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
