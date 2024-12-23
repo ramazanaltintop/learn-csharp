8a5639122d5ed9fc16c081a9753168744cbe5b6b
Ramazan Altintop <ramazan.altintop@yahoo.com>
2024-12-23 12:59:24 +0300

    Test Attributes 2 completed!

---
+++ TestDrivenDevelopment/TestAttributes2/TestAttributes2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAttributes2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAttributes2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a1f26d1-175f-40a2-83a1-027a8062e6e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestDrivenDevelopment/TestAttributes2/TestAttributes2.Tests/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace TestAttributes2.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, Description("Bu bir karekök testi yapan metottur.")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Ignore]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Timeout(1000)]
+        public void TestMethod3()
+        {
+            Thread.Sleep(1000);
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Ignore]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod, Timeout(TestTimeout.Infinite)]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
