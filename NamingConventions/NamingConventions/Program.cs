04a8ec76bd853e36781bacc73f9202466910a2fd
Ramazan Altintop <ramazan.altintop@yahoo.com>
2025-01-19 13:33:21 +0300

    Updated

---
+++ NamingConventions/NamingConventions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConventions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ProductManager
+    {
+        // fields
+        public int LargestNumber;
+
+        private int smallestNumber;
+
+        // properties
+
+        public int SmallestNumber { get; set; }
+
+        // constructor
+        public ProductManager()
+        {
+            
+        }
+
+        // method
+        public void GetAllByCategoryId(int categoryId)
+        {
+            // local variable
+            int anyNumber;
+        }
+    }
+}
